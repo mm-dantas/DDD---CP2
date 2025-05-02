8419ddb9d8571deec7f2d455c3bc4f13753a7360
mm-dantas <matheusgithub@outlook.com>
2025-05-02 00:52:00 -0300

    adicionando cliente e produto

---
+++ src/br/com/lanchonete/Produto.java
@@ -0,0 +1,47 @@
+package br.com.lanchonete;
+
+public class Produto {
+    private String nome;
+    private double preco;
+    private String categoria;
+
+    public Produto(String nome){
+        this.nome = nome;
+    }
+
+    public Produto(String nome, double preco){
+        this.nome = nome;
+        this.preco = preco;
+    }
+
+    public Produto(String nome, double preco, String categoria){
+        this.nome = nome;
+        this.preco = preco;
+        this.categoria = categoria;
+    }
+
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
+}
